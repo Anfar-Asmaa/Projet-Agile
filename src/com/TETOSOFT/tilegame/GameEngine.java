469cf9c6de3f082672c63598d5567cf43775e8a4
kingridda <abdelghaniridda01@gmail.com>
2021-05-04 00:32:33 +0000

    bf

--- src/com/TETOSOFT/tilegame/GameEngine.java
+++ src/com/TETOSOFT/tilegame/GameEngine.java
@@ -47,7 +47,7 @@ public class GameEngine extends GameCore
         
         // load resources
         drawer = new TileMapDrawer();
-        drawer.setBackground(mapLoader.loadImage("background3.png"));
+        drawer.setBackground(mapLoader.loadImage("background.png"));
         
         // load first map
         map = mapLoader.loadNextMap();
